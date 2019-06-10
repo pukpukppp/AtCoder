ed657a7aacf5a189f8a47d287702687cec5ebb2c
pukpukppp <46735754+pukpukppp@users.noreply.github.com>
2019-06-11 03:42:13 +0900

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -8,46 +8,31 @@ using namespace std;
 int main(){
   int N,M;
   cin >> N >> M;
-  vector<int> f(N,0);
-  vector<int> a(M,0);
+  vector<long long int> f(N + 1,0);
+  vector<int> a(M + 1,0);
 
   for(int i = 0;i < M;i++){
     cin >> a[i];
-    a[i] -= 1;
-    cout << a[i];
   }
   
-  int temp1,temp2;
-
-  f[0] = 1;
-  f[1] = 2;
-
-  temp1 = f[0];
-  temp2 = f[1];
-
+  long long int temp1,temp2; 
   int j = 0;
 
-  //1,2段目が抜けていた場合
-  if(a[0] == 0){
-    f[0] = 0;
-    j++;
-  } 
-  if(a[0] == 1 || a[1] == 1){
-    f[1] = 0;
-    j++;
-  }
+  f[0] = 1;
+  temp1 = f[0];
+  temp2 = 0;
 
-  for(int i = 2;i < N;i++){
+  for(int i = 1;i < N + 1;i++){
     //f[i] がaと一致しない場合は行う
-    if(f[i] != a[j]){
+    if(i != a[j]){
       f[i] = temp1 + temp2;
-      temp2 = f[i - 1];
-      temp1 = f[i];
     }else{
       j++;
-      cout <<"TT"
     }
+    temp2 = f[i - 1];
+    temp1 = f[i];
+    f[i] %= 1000000007;
   }
 
-  cout << f[N - 1] << endl;
+  cout << f[N] << endl;
 }
